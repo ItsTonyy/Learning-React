f7e1a547cdac588f059a02f687531d684f544b9d
Tony <tonytnportela@outlook.com>
2023-05-13 16:02:40 -0300

    added jsx exercise

---
+++ learning-jsx/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  const name = 'Tony'
+  const url = 'https://via.placeholder.com/150'
+
+  return (
+    <div className='app'>
+      <h1>Isto é um H1</h1>
+      <h2>Testando interpolação usando variáveis</h2>
+      <p>olá, {name}. Como vai?</p>
+      <p>soma: {53 * 432}</p>
+      <p>divisão: {432 / 52}</p>
+      <img src={url} alt="imagem"/>
+    </div>
+  )
+}
+
+export default App
