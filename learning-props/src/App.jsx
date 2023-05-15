f8fff00b96a80689910357ca6ffe43605eda0d73
Tony <tonytnportela@outlook.com>
2023-05-14 23:55:13 -0300

    Added Props lesson

---
+++ learning-props/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/pessoa'
+
+function App() {
+  const url = 'https://via.placeholder.com/150'
+  const nome = 'maria'
+  
+  return (
+    <div className='app'>
+      <HelloWorld />
+      <SayMyName nome='Tony' />
+      <SayMyName nome='Julia' />
+      <SayMyName nome={nome} />
+      <Pessoa 
+        nome='Tony'
+        idade = '19'
+        profissão = 'programador'
+        foto = {url} />
+    </div>
+  )
+}
+
+export default App
