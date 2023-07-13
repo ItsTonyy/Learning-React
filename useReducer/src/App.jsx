3bcf906d80b858447c4c05e00c5c70e6d38676a5
Tony <tonytnportela@outlook.com>
2023-07-12 23:43:42 -0300

    useReducer Finished

--- useReducer/src/App.jsx
+++ useReducer/src/App.jsx
@@ -1,31 +1,78 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 
 const reducer = (state, action) => {
 	switch (action.type) {
-		case 'increment': { 
+		case 'add-task':
 			return {
-				counter: state.counter + 1,
+				...state,
+				tasks: [...state.tasks, { name: action.payload, isCompleted: false }],
+				taskCount: state.taskCount + 1,
 			}
-    }
-		case 'decrement': { 
+		case 'toogle-task':
 			return {
-				counter: state.counter - 1,
+				...state,
+				tasks: state.tasks.map((item, index) =>
+					index === action.payload
+						? { ...item, isCompleted: !item.isCompleted }
+						: item
+				),
 			}
-    }
-		default: { 
+		case 'clear-tasks':
+			return {
+				tasks: []
+			}
+		default:
 			return state
-    }
 	}
 }
 
 function App() {
-	const [state, dispatch] = useReducer(reducer, { counter: 0 })
+	const [state, dispatch] = useReducer(reducer, { tasks: [], taskCount: 0 })
+
+	const [inputValue, setInputValue] = useState('')
 
 	return (
-		<div>
-			<p>{state.counter}</p>
-			<button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
-      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+		<div className='main-div'>
+			<h1>Use useReduce when your state is too complex</h1>
+
+			<div className='controller'>
+				<input
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+				/>
+				<button
+					onClick={() => {
+						dispatch({ type: 'add-task', payload: inputValue })
+						setInputValue('')
+					}}
+				>
+					Add
+				</button>
+				<button
+					onClick={() => {
+						dispatch({ type: 'clear-tasks', payload: state })
+					}}
+				>
+					clear
+				</button>
+				<button className='number-tasks'>
+					Number of Tasks: {state.taskCount}
+				</button>
+			</div>
+
+			{state.tasks.map((task, index) => (
+				// eslint-disable-next-line react/jsx-key
+				<div className='outer-box'>
+					<p
+						onClick={() => dispatch({ type: 'toogle-task', payload: index })}
+						style={{
+							textDecoration: task.isCompleted ? 'line-through' : 'none',
+						}}
+					>
+						{task.name}
+					</p>
+				</div>
+			))}
 		</div>
 	)
 }
